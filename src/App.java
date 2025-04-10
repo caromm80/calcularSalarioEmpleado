4507c7dc2f6803bfbca58e02e66d3b42191c8714
caromm80 <caromm80@gmail.com>
2025-04-10 16:44:51 -0500

    se finaliza ejercicio

--- src/App.java
+++ src/App.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         Scanner sc = new Scanner(System.in);
         String nombreEmpleado, documentoEmpleado, diaDescanso = "";
         double salarioBruto = 0;
@@ -22,22 +21,50 @@ public class App {
         System.out.println("Ingrese el documento del empleado: ");
         documentoEmpleado = sc.nextLine();
         System.out.println("Ingrese el dia de descanso del empleado: ");
-        diaDescanso = sc.nextline();
-        System.out.println("Ingrese el salario bruto del empleado: ");
-        salarioBruto = sc.nextDouble();
-        System.out.println("Ingrese el salario neto del empleado: ");
-        salarioNeto = sc.nextDouble();
-        System.out.println("Ingrese la deduccion de pension del empleado: ");
-        deduccionPension = sc.nextDouble();
-        System.out.println("Ingrese la deduccion de salud del empleado: ");
-        deduccionSalud = sc.nextDouble();
+        diaDescanso = sc.nextLine();
+    
+   
         System.out.println("Ingrese las horas trabajadas del empleado: ");
         horasTrabajadas = sc.nextDouble();
         System.out.println("Ingrese el valor de la hora del empleado: ");
         valorHora = sc.nextDouble();
-        System.out.println("Ingrese la bonificacion del empleado: ");
-        bonificacionEmpleado = sc.nextDouble();
-        System.out.println(Ingrese el valor del salario minimo);
+        
+        
+        salarioBruto = horasTrabajadas * valorHora;
+        if (salarioBruto<= 2*valorSalarioMinimo) {
+            auxilioTransporte = 200000;
+            bonificacionEmpleado = salarioBruto*0.1;
+
+            
+        } else{
+            auxilioTransporte = 0;
+            bonificacionEmpleado = 0;
+        }
+        deduccionPension = salarioBruto * 0.04;
+        deduccionSalud = salarioBruto * 0.04;
+
+        if (horasTrabajadas>96) {
+            cantidadHorasExtras = horasTrabajadas - 96;
+            valorHoraExtra = valorHora * 1.25;
+            totalHorasExtras = cantidadHorasExtras * valorHoraExtra;
+        }
+        if (horasTrabajadas < 24) {
+            System.out.println("Aún no le puedo pagar, trabaje más");
+        }
+            else{
+                salarioNeto=salarioBruto-deduccionPension-deduccionSalud+bonificacionEmpleado+auxilioTransporte+totalHorasExtras;
+                System.out.println("El salario neto del empleado es:" + salarioNeto);
+                System.out.println("El auxilio de transporte es:" + auxilioTransporte);
+                System.out.println("Deducción por salud es:" + deduccionSalud);
+                System.out.println("Deducción por pensión es:" + deduccionPension);
+                System.out.println("Las horas extras equivalen a:" + totalHorasExtras);
+                
+
+            }
+            
+
+            
+        }
 
 
     }
